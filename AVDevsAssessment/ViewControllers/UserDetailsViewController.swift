6fcc33128aaa2e15e76c739f3a8c73ded43b5f64
Keyur Patel <patelkeyur7279@gmail.com>
2022-12-18 04:14:24 +0530

    User Email Address Tap Enabled

--- AVDevsAssessment/ViewControllers/UserDetailsViewController.swift
+++ AVDevsAssessment/ViewControllers/UserDetailsViewController.swift
@@ -22,11 +22,27 @@ class UserDetailsViewController: UIViewController {
         userImageView.image = UIImage(named: user.gender == .male ? "male" : "female")
         userStatusImageView.tintColor = user.status == .active ? #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1) : #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         userName.text = user.name
-        userEmailAddress.text = user.email
         
-        print("User: \(user)")
+        userEmailAddress.text = user.email
+        userEmailAddress.delegate = self
+        
         navigationItem.title = user.name
         
+    }
+    
+}
+
+// Note: I'm not able to test below code in simulator So, if anything wrong with below code then please ignore And nothing happens with requested functionality
+extension UserDetailsViewController: UITextViewDelegate {
+    
+    func textView(_ textView: UITextView, shouldInteractWith url: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        print("URL: \(url)")
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+        return true
     }
     
 }
